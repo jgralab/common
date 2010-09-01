77a495124d12586ad82ada1339b0c925dc62f8ac
Sascha Strauß <strauss@uni-koblenz.de>
2010-09-01 13:03:12 +0000

    made "common" a real java project. Removed ist_utilities.jar from svn. All eclipse projects depending on "ist_utilities.jar" now have to depend directly on "common". This includes all branches of JGraLab. The new file "build.xml" is the build file for common. It creates the ist_utilities.jar in the old location. It does not depend on common.xml.

--- ist_utilities/src/de/uni_koblenz/ist/utilities/auto_build_id/AutoBuildID.java
+++ ist_utilities/src/de/uni_koblenz/ist/utilities/auto_build_id/AutoBuildID.java
@@ -23,9 +23,6 @@
 
 package de.uni_koblenz.ist.utilities.auto_build_id;
 
-import gnu.getopt.Getopt;
-import gnu.getopt.LongOpt;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -36,155 +33,106 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+
+import de.uni_koblenz.ist.utilities.option_handler.OptionHandler;
+
 public class AutoBuildID {
 
-	private LongOpt[] longOptions;
+	private String filename;
 
 	// look but don't touch, both values are updated automatically
 	// private final String revision = "$Revision: 4522 $";
-	// private final String buildID = "30";
+	// private final String buildID = "31";
 	//
 	// to use this information inside the text place $rev for the revision
 	// information
 	// and $bid for the build id
 
-	private final String[] info = { "Helptext", "TODO!" };
-
-	public AutoBuildID(String[] args) {
-		createLongOptions();
-		processArguments(args);
+	public AutoBuildID(String filename) {
+		this.filename = filename;
 	}
 
-	private void createLongOptions() {
-		longOptions = new LongOpt[2];
+	private static CommandLine processCommandLine(String[] args) {
+		// Creates a OptionHandler.
+		String toolString = "java " + AutoBuildID.class.getName();
+		String versionString = "1.0";
+		OptionHandler oh = new OptionHandler(toolString, versionString);
+		Option modify = new Option("m", "modify", true,
+				"(required): Modifies the given Java source file.");
+		modify.setRequired(true);
+		modify.setArgName("filename");
+		oh.addOption(modify);
 
-		longOptions[0] = new LongOpt("modify", LongOpt.REQUIRED_ARGUMENT, null,
-				'm');
-		longOptions[1] = new LongOpt("info", LongOpt.REQUIRED_ARGUMENT, null,
-				'i');
-	}
-
-	private void processArguments(String[] args) {
-		Getopt getopt = new Getopt("AutoBuildID", args, "im:", longOptions);
-		int option;
-		String optionArg;
-
-		/*
-		 * if no command line arguments are specified, create an "-i" argument
-		 * inorder to invoke printInfo()
-		 */
-		if (args.length == 0) {
-			getopt.setArgv(new String[] { "-i" });
-		}
-
-		while ((option = getopt.getopt()) != -1) {
-			switch (option) {
-			case 'i':
-				printInfo();
-				break;
-			case 'm':
-				optionArg = getopt.getOptarg();
-				File inputFile = new File(optionArg);
-				System.out.println("Input-file is " + inputFile.toString());
-				File outputFile = new File(optionArg + ".out");
-				FileReader fr;
-				FileWriter fw;
-				BufferedReader br;
-				BufferedWriter bw;
-				ArrayList<String> listOfFile = new ArrayList<String>(250);
-
-				try {
-					fr = new FileReader(inputFile);
-					br = new BufferedReader(fr);
-					String line;
-
-					// System.out.println("Reading input file.");
-
-					// copy the file to a list by line
-					while ((line = br.readLine()) != null) {
-						listOfFile.add(line);
-					}
-
-					br.close();
-					fr.close();
-
-					// System.out.println("Input file read.");
-
-					// write the whole list into a new file
-					fw = new FileWriter(outputFile);
-					bw = new BufferedWriter(fw);
-
-					// System.out.println("Checking for build-ID and writing tempfile.");
-
-					ListIterator<String> i = listOfFile.listIterator();
-					String[] subLines;
-					int wl = 0;
-					while (i.hasNext()) {
-						line = i.next();
-						wl++;
-						// check for the build-id and update it
-						if (line.matches(".*String buildID = \"[0-9]+\";$")) {
-							subLines = line.split("\"");
-							System.out.println("Changed build-ID in line " + wl
-									+ ".");
-							int newBid = Integer.parseInt(subLines[1]) + 1;
-							line = subLines[0] + "\"" + newBid + "\""
-									+ subLines[2];
-						}
-
-						bw.write(line, 0, line.length());
-						bw.newLine();
-					}
-
-					bw.close();
-					fw.close();
-
-					inputFile.delete();
-					outputFile.renameTo(inputFile);
-					outputFile.deleteOnExit();
-					// System.out.println("Input file changed.");
-				}
-
-				catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-
-				}
-
-				break;
-			}
-		}
+		return oh.parse(args);
 	}
 
 	public static void main(String[] args) {
-		new AutoBuildID(args);
+		CommandLine cl = processCommandLine(args);
+		AutoBuildID abid = new AutoBuildID(cl.getOptionValue("m"));
+		abid.process();
 	}
 
-	private void printInfo() {
-		System.out.println(toString());
-	}
+	public void process() {
+		File inputFile = new File(filename);
+		System.out.println("Auto build ID");
+		System.out.println("Input-file is " + inputFile.toString());
+		File outputFile = new File(filename + ".out");
+		FileReader fr;
+		FileWriter fw;
+		BufferedReader br;
+		BufferedWriter bw;
+		ArrayList<String> listOfFile = new ArrayList<String>(250);
 
-	// private String addInfo(String inputLine){
-	// String outputLine = inputLine;
-	// String revString = revision.replace("$R","R").replace(" $","");
-	// 		
-	// outputLine = outputLine.replace("$rev",revString);
-	// outputLine = outputLine.replace("$bid","Build ID: " + buildID);
-	// 		
-	// return outputLine;
-	// }
+		try {
+			fr = new FileReader(inputFile);
+			br = new BufferedReader(fr);
+			String line;
+			
+			// copy the file to a list by line
+			while ((line = br.readLine()) != null) {
+				listOfFile.add(line);
+			}
 
-	@Override
-	public String toString() {
-		StringBuffer output = new StringBuffer(1024);
-		output.append('\n');
-		for (int i = 0; (i < info.length); i++) {
-			output.append(' ');
-			// output.append(addInfo(info[i]));
-			output.append(info[i]);
-			output.append('\n');
+			br.close();
+			fr.close();
+			
+			// write the whole list into a new file
+			fw = new FileWriter(outputFile);
+			bw = new BufferedWriter(fw);
+
+			ListIterator<String> i = listOfFile.listIterator();
+			String[] subLines;
+			int wl = 0;
+			while (i.hasNext()) {
+				line = i.next();
+				wl++;
+				// check for the build-id and update it
+				if (line.matches(".*String buildID = \"[0-9]+\";$")) {
+					subLines = line.split("\"");
+					System.out.println("Changed build-ID in line " + wl + ".");
+					int newBid = Integer.parseInt(subLines[1]) + 1;
+					line = subLines[0] + "\"" + newBid + "\"" + subLines[2];
+				}
+
+				bw.write(line, 0, line.length());
+				bw.newLine();
+			}
+
+			bw.close();
+			fw.close();
+
+			inputFile.delete();
+			outputFile.renameTo(inputFile);
+			outputFile.deleteOnExit();
+			System.out.println("Fini.");
 		}
-		return output.toString();
+
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
